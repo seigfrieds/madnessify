c4eee9d7f710acf465f087e7dcf68dbec2cea16f
Andre <andreulep@gmail.com>
2023-04-27 22:20:44 -0500

    change redirect URI

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
     authEndpoint: "https://accounts.spotify.com/authorize",
     clientId: "1f533f0d8499465683ccbfe57c8fc54b",
-    redirectUri: "http://localhost:3000/",
+    redirectUri: "https://seigfrieds.github.io/music-madness/",
     responseType: "token",
     scope: "user-top-read"
 };
