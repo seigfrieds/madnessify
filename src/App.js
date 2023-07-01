3896e5c0f26378fc1b5acb3657697c5546768a27
Andre <106368574+seigfrieds@users.noreply.github.com>
2023-06-30 21:17:21 -0500

    Remove development message
    App is now accessible to public!

--- src/App.js
+++ src/App.js
@@ -161,7 +161,6 @@ function App() {
                     <h1 id="app-title">Madnessify!</h1>
 
                     <h2 id="app-hook">Create a single-elimination bracket tournament out of your favorite songs on Spotify!</h2><br/>
-                    <h2 id="special-notice">In Development Mode - Need special access to use the site</h2><br/>
 
                     <iframe width="420" height="315"
                     src="https://www.youtube.com/embed/IMyBgxkcuw4">
