ffb7990847c1d45b52830aa6c66646912161b9d7
Andre <106368574+seigfrieds@users.noreply.github.com>
2023-12-26 17:45:20 -0600

    Extract out spotify api requests

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,8 +1,7 @@
-import React, { useState, FormEvent, ChangeEvent, ReactElement } from "react";
+import React, { FormEvent } from "react";
 import BracketParameterForm from "./BracketParameterForm/BracketParameterForm";
-import axios from "axios";
 import { useNavigate, useLocation } from "react-router-dom";
-import { Song } from "../../types";
+import { getPlaylistTracks, getTopTracks } from "./spotifyService";
 
 type QueryData = {
   query?: string;
@@ -11,97 +10,20 @@ type QueryData = {
   playlistLink?: string;
 };
 
-//durstenfeld shuffle: https://stackoverflow.com/a/12646864
-const shuffleArray = (array: Array<any>): void => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-};
-
 export default function HomePage(): React.JSX.Element {
   const { state } = useLocation();
   const { token } = state;
   const navigate = useNavigate();
 
-  async function getTopTracks(
+  async function getTracks(
     token: string,
-    numTracks: number | undefined,
-    timeFrame: string | undefined,
+    { query, numTracks, timeFrame, playlistLink }: QueryData,
   ): Promise<void> {
-    if (numTracks && timeFrame) {
-      const request = await axios.get("https://api.spotify.com/v1/me/top/tracks", {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-        params: {
-          limit: numTracks,
-          time_range: timeFrame,
-        },
-      });
-
-      shuffleArray(request.data.items);
-
-      return request.data.items;
-    }
-  }
-
-  async function getPlaylistSize(playlistId: string) {
-    const req = await axios.get(`https://api.spotify.com/v1/playlists/${playlistId}`, {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-      params: {
-        fields: "tracks",
-      },
-    });
-
-    return req.data.tracks.total;
-  }
-
-  async function getPlaylistTracks(
-    token: string,
-    numTracks: number | undefined,
-    playlistLink: string | undefined,
-  ): Promise<void> {
-    if (numTracks && playlistLink) {
-      const PLAYLIST_ID = playlistLink.split("/playlist/")[1].split("?si=")[0];
-
-      let playlistSize = 0;
-      await getPlaylistSize(PLAYLIST_ID).then((result) => {
-        playlistSize = result;
-      });
-
-      if (playlistSize < numTracks) {
-        alert(`Error: Playlist does not have ${numTracks} tracks`);
-      } else {
-        const startingPosition = Math.floor(Math.random() * playlistSize - 100);
-
-        const selectedSection = await axios.get(
-          `https://api.spotify.com/v1/playlists/${PLAYLIST_ID}/tracks?offset=${
-            startingPosition > 0 ? startingPosition : 0
-          }&limit=100&locale=en-US,en;q=0.9`,
-          {
-            headers: {
-              Authorization: "Bearer " + token,
-            },
-          },
-        );
-
-        shuffleArray(selectedSection.data.items);
-
-        return selectedSection.data.items
-          .slice(0, numTracks)
-          .map((item: { track: Song }) => item.track);
-      }
-    }
-  }
-
-  async function getTracks(token: string, data: QueryData): Promise<void> {
-    if (data.query === "top_tracks") {
-      return getTopTracks(token, data.numTracks, data.timeFrame);
-    } else if (data.query === "playlist") {
-      return getPlaylistTracks(token, data.numTracks, data.playlistLink);
+    switch (query) {
+      case "top_tracks":
+        return getTopTracks(token, numTracks, timeFrame);
+      case "playlist":
+        return getPlaylistTracks(token, numTracks, playlistLink);
     }
   }
 
