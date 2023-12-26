ba7dfe3c41d95b8f5226849aeafb2fbc1bc4cb5c
Andre <106368574+seigfrieds@users.noreply.github.com>
2023-12-26 15:56:01 -0600

    refactor HomePage
    refactored home page +
    
    do not keep track of form in useState -> only care about it on submit

--- src/pages/Home/BracketParameterForm/BracketParameterForm.tsx
+++ src/pages/Home/BracketParameterForm/BracketParameterForm.tsx
@@ -3,85 +3,80 @@ import React, { useState } from "react";
 
 type Props = {
   handleSubmit: React.FormEventHandler<HTMLFormElement>;
-  handleQueryTypeChange: React.ChangeEventHandler<HTMLSelectElement>;
-  handleNumTracksChange: React.ChangeEventHandler<HTMLSelectElement>;
-  handleTimeFrameChange: React.ChangeEventHandler<HTMLSelectElement>;
-  handlePlaylistLinkChange: React.ChangeEventHandler<HTMLInputElement>;
 };
 
-function BracketParameterForm({
-  handleSubmit,
-  handleQueryTypeChange,
-  handleNumTracksChange,
-  handleTimeFrameChange,
-  handlePlaylistLinkChange,
-}: Props): React.JSX.Element {
+function BracketParameterForm({ handleSubmit }: Props): React.JSX.Element {
   const [currQuery, setCurrQuery] = useState("");
 
   function changeQuery(event: React.ChangeEvent<HTMLSelectElement>): void {
-    handleQueryTypeChange(event);
     setCurrQuery(event.target.value);
   }
 
   return (
     <div className="FormScreen">
-      <div className="dropdown">
-        <label htmlFor="query-select">Select where you want your songs to come from!</label>
-        <br></br>
-        <select onChange={changeQuery} id="query-select" form="theform">
-          <option value="" selected disabled hidden>
-            Choose...
-          </option>
-          <option value={"top_tracks"}>Top Tracks</option>
-          <option value={"playlist"}>Playlist</option>
-        </select>
-      </div>
-
-      {currQuery === "top_tracks" && (
-        <>
-          <div className="dropdown">
-            <label htmlFor="num-songs-select">Select the number of songs: </label>
-            <br></br>
-            <select onChange={handleNumTracksChange} id="num-songs-select" form="theform">
-              <option value={8}>Top 8 songs</option>
-              <option value={16}>Top 16 songs</option>
-              <option value={32}>Top 32 songs</option>
-            </select>
-          </div>
-
-          <div className="dropdown">
-            <label htmlFor="time-frame-select">Select the time frame: </label>
-            <br></br>
-            <select onChange={handleTimeFrameChange} id="time-frame-select" form="theform">
-              <option value={"short_term"}>Past 4 weeks</option>
-              <option value={"medium_term"}>Past 6 months</option>
-              <option value={"long_term"}>Past several years</option>
-            </select>
-          </div>
-        </>
-      )}
-
-      {currQuery === "playlist" && (
-        <>
-          <div id="playlist-prompt">
-            <label htmlFor="playlist-prompt">Enter the link to your playlist:</label>
-            <br></br>
-            <input type="text" id="playlist-box" onChange={handlePlaylistLinkChange}></input>
-          </div>
-
-          <div className="dropdown">
-            <label htmlFor="num-songs-select">Select the number of songs: </label>
-            <br></br>
-            <select onChange={handleNumTracksChange} id="num-songs-select" form="theform">
-              <option value={8}>8 random songs</option>
-              <option value={16}>16 random songs</option>
-              <option value={32}>32 random songs</option>
-            </select>
-          </div>
-        </>
-      )}
-
       <form onSubmit={handleSubmit} id="theform">
+        <div className="dropdown">
+          <label htmlFor="query-select">Select where you want your songs to come from!</label>
+          <br></br>
+          <select onChange={changeQuery} id="query-select" name="queryselect">
+            <option value="" selected disabled hidden>
+              Choose...
+            </option>
+            <option value={"top_tracks"}>Top Tracks</option>
+            <option value={"playlist"}>Playlist</option>
+          </select>
+        </div>
+
+        {currQuery === "top_tracks" && (
+          <>
+            <div className="dropdown">
+              <label htmlFor="num-songs-select">Select the number of songs: </label>
+              <br></br>
+              <select id="num-songs-select" name="numsongsselect">
+                <option selected value={8}>
+                  Top 8 songs
+                </option>
+                <option value={16}>Top 16 songs</option>
+                <option value={32}>Top 32 songs</option>
+              </select>
+            </div>
+
+            <div className="dropdown">
+              <label htmlFor="time-frame-select">Select the time frame: </label>
+              <br></br>
+              <select id="time-frame-select" name="timeframeselect">
+                <option selected value={"short_term"}>
+                  Past 4 weeks
+                </option>
+                <option value={"medium_term"}>Past 6 months</option>
+                <option value={"long_term"}>Past several years</option>
+              </select>
+            </div>
+          </>
+        )}
+
+        {currQuery === "playlist" && (
+          <>
+            <div id="playlist-prompt">
+              <label htmlFor="playlist-prompt">Enter the link to your playlist:</label>
+              <br></br>
+              <input type="text" id="playlist-box" name="playlistbox"></input>
+            </div>
+
+            <div className="dropdown">
+              <label htmlFor="num-songs-select">Select the number of songs: </label>
+              <br></br>
+              <select id="num-songs-select" name="numsongsselect">
+                <option selected value={8}>
+                  8 random songs
+                </option>
+                <option value={16}>16 random songs</option>
+                <option value={32}>32 random songs</option>
+              </select>
+            </div>
+          </>
+        )}
+
         <button id="submit-button" type="submit">
           Submit!
         </button>
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -3,26 +3,32 @@ import BracketParameterForm from "./BracketParameterForm/BracketParameterForm";
 import axios from "axios";
 import { useNavigate, useLocation } from "react-router-dom";
 
+type QueryData = {
+  query?: string;
+  numTracks?: number;
+  timeFrame?: string;
+  playlistLink?: string;
+};
+
 //durstenfeld shuffle: https://stackoverflow.com/a/12646864
-function shuffleArray(array: Array<any>): void {
+const shuffleArray = (array: Array<any>): void => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
-}
+};
 
 export default function HomePage(): React.JSX.Element {
   const { state } = useLocation();
   const { token } = state;
-  const [queryType, setQueryType] = useState(""); //top_tracks, playlist
-  const [numTracks, setNumTracks] = useState(8);
-  const [timeFrame, setTimeFrame] = useState("short_term");
-  const [playlistLink, setPlaylistLink] = useState("");
-  const [error, setError] = useState<null | ReactElement>(null);
   const navigate = useNavigate();
 
-  async function getTracks(token: string): Promise<void> {
-    if (queryType === "top_tracks") {
+  async function getTopTracks(
+    token: string,
+    numTracks: number | undefined,
+    timeFrame: string | undefined,
+  ): Promise<void> {
+    if (numTracks && timeFrame) {
       const request = await axios.get("https://api.spotify.com/v1/me/top/tracks", {
         headers: {
           Authorization: "Bearer " + token,
@@ -36,47 +42,44 @@ export default function HomePage(): React.JSX.Element {
       shuffleArray(request.data.items);
 
       return request.data.items;
-    } else if (queryType === "playlist") {
+    }
+  }
+
+  async function getPlaylistSize(playlistId: string) {
+    const req = await axios.get(`https://api.spotify.com/v1/playlists/${playlistId}`, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+      params: {
+        fields: "tracks",
+      },
+    });
+
+    return req.data.tracks.total;
+  }
+
+  async function getPlaylistTracks(
+    token: string,
+    numTracks: number | undefined,
+    playlistLink: string | undefined,
+  ): Promise<void> {
+    if (numTracks && playlistLink) {
       const PLAYLIST_ID = playlistLink.split("/playlist/")[1].split("?si=")[0];
 
-      //total # of tracks in the playlist
-      let totalTracksInPlaylist = 0;
+      let playlistSize = 0;
+      await getPlaylistSize(PLAYLIST_ID).then((result) => {
+        playlistSize = result;
+      });
 
-      //spotify playlists have id's of length 22 -> enforce this check to reduce API spam
-      if (PLAYLIST_ID.length === 22) {
-        //get playlist (Defaults to first 100 tracks)
-        const request = await axios.get("https://api.spotify.com/v1/playlists/" + PLAYLIST_ID, {
-          headers: {
-            Authorization: "Bearer " + token,
-          },
-          params: {
-            fields: "tracks",
-          },
-        });
-
-        totalTracksInPlaylist = request.data.tracks.total;
-      }
-
-      //if # of tracks in playlist does not accomodate chosen bracket size
-      if (totalTracksInPlaylist < numTracks) {
-        setError(<p id="error">Error! Not enough tracks in playlist</p>);
+      if (playlistSize < numTracks) {
+        alert(`Error: Playlist does not have ${numTracks} tracks`);
       } else {
-        //calculate # of sections with 100 songs in playlist (e.g. songs 1-99, songs 300-399)
-        const NUM_OF_100_SONG_SECTIONS_IN_PLAYLIST = Math.ceil(totalTracksInPlaylist / 100);
+        const startingPosition = Math.random() * playlistSize - 100;
 
-        //pick random 100 song section in playlist
-        let selectedSectionNum = Math.floor(Math.random() * NUM_OF_100_SONG_SECTIONS_IN_PLAYLIST);
-        //while randomly selected section cannot accomodate bracket
-        while (totalTracksInPlaylist - selectedSectionNum * 100 < numTracks)
-          selectedSectionNum = Math.floor(Math.random() * NUM_OF_100_SONG_SECTIONS_IN_PLAYLIST);
-
-        //get random 100 song section in playlist from api
         const selectedSection = await axios.get(
-          "https://api.spotify.com/v1/playlists/" +
-            PLAYLIST_ID +
-            "/tracks?offset=" +
-            selectedSectionNum * 100 +
-            "&limit=100&locale=en-US,en;q=0.9",
+          `https://api.spotify.com/v1/playlists/${PLAYLIST_ID}/tracks?offset=${
+            startingPosition > 0 ? startingPosition : 0
+          }&limit=100&locale=en-US,en;q=0.9`,
           {
             headers: {
               Authorization: "Bearer " + token,
@@ -84,7 +87,6 @@ export default function HomePage(): React.JSX.Element {
           },
         );
 
-        /* get tracks! */
         shuffleArray(selectedSection.data.items);
 
         const chosenPlaylistTracks = [];
@@ -96,39 +98,30 @@ export default function HomePage(): React.JSX.Element {
     }
   }
 
-  function handleSubmit(event: FormEvent): void {
+  async function getTracks(token: string, data: QueryData): Promise<void> {
+    if (data.query === "top_tracks") {
+      return getTopTracks(token, data.numTracks, data.timeFrame);
+    } else if (data.query === "playlist") {
+      return getPlaylistTracks(token, data.numTracks, data.playlistLink);
+    }
+  }
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>): void {
     event.preventDefault();
 
-    getTracks(token).then((result) => navigate("/tournament", { state: { players: result } }));
+    const target = event.currentTarget;
+
+    const data = {
+      query: target.queryselect?.value,
+      numTracks: Number(target.numsongsselect?.value),
+      timeFrame: target.timeframeselect?.value,
+      playlistLink: target.playlistbox?.value,
+    };
+
+    getTracks(token, data).then((result: any) => {
+      if (result) navigate("/tournament", { state: { players: result } });
+    });
   }
 
-  function handlePlaylistLinkChange(event: ChangeEvent<HTMLInputElement>): void {
-    console.log(playlistLink);
-    setPlaylistLink(event.target.value);
-  }
-
-  function handleQueryTypeChange(event: ChangeEvent<HTMLSelectElement>): void {
-    setQueryType(event.target.value);
-  }
-
-  function handleNumTracksChange(event: ChangeEvent<HTMLSelectElement>): void {
-    setNumTracks(Number(event.target.value));
-  }
-
-  function handleTimeFrameChange(event: ChangeEvent<HTMLSelectElement>): void {
-    setTimeFrame(event.target.value);
-  }
-
-  return (
-    <>
-      <BracketParameterForm
-        handleSubmit={handleSubmit}
-        handleQueryTypeChange={handleQueryTypeChange}
-        handleNumTracksChange={handleNumTracksChange}
-        handleTimeFrameChange={handleTimeFrameChange}
-        handlePlaylistLinkChange={handlePlaylistLinkChange}
-      />
-      {error !== null && error}
-    </>
-  );
+  return <BracketParameterForm handleSubmit={handleSubmit} />;
 }
