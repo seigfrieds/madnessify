b52f238224da1f03d2de4c36b142fc5ccab151ee
Andre <106368574+seigfrieds@users.noreply.github.com>
2023-12-28 09:19:13 -0600

    allow connection between front and back

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const app = express();
+app.use(cors());
 
 app.get("/api", (req, res) => {
   res.send("HELLO!");
