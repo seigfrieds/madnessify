55d54a34d4b2022303239c5ff2a72aaa86e1f981
Andre <106368574+seigfrieds@users.noreply.github.com>
2024-01-04 04:12:36 -0600

    Setup bracket routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import authRouter from "./routes/oauth.routes.js";
+import bracketRouter from "./routes/bracket.routes.js";
 import pool from "./db.js";
 
 dotenv.config();
@@ -13,6 +14,7 @@ app.use(express.json()); //get req.body
 
 //routes
 app.use("/oauth", authRouter);
+app.use("/api/bracket", bracketRouter);
 
 app.get("/api", (req, res) => {
   res.send("HELLO!");
