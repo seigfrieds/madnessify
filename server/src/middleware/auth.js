cc514fe843339a1e6e4142cfe03647e5104e0177
Andre <106368574+seigfrieds@users.noreply.github.com>
2024-01-07 04:31:14 -0600

    style: clarifying comment

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -14,6 +14,7 @@ const auth = async (req, res, next) => {
       return res.status(401).json({ message: err.message });
     }
 
+    //check if session uuid is in the cache
     const validSession = await cache.get(payload.session);
 
     if (validSession) {
