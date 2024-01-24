4cd50ad0ad11d84a4ce9022291a83e35c19dc138
Andre <106368574+seigfrieds@users.noreply.github.com>
2024-01-23 21:34:26 -0600

    selectively display results
    user searches something -> it shows --> if they click outside search area, it disappears. If they click back on the search box, it will show up again

--- client/src/pages/Home/components/SearchBox.tsx
+++ client/src/pages/Home/components/SearchBox.tsx
@@ -1,32 +1,71 @@
 import "./SearchBox.css";
-import React, { ReactElement, useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 type Props = {
   searchFnc: (query: string) => Promise<void>;
-  mapFnc: (item: any) => ReactElement;
+  mapFnc: (item: any) => React.ReactElement;
 };
 
 function SearchBox({ searchFnc, mapFnc }: Props): React.JSX.Element {
   const [searchResults, setSearchResults] = useState(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const promptRef = useRef<HTMLInputElement>(null);
+  const resultsRef = useRef<HTMLInputElement>(null);
 
-  const onSearchChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const query = event.target.value;
+  const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const query = e.target.value;
 
     if (query.length === 0) {
       setSearchResults(null);
     } else {
-      searchFnc(event.target.value).then((res) => {
+      searchFnc(e.target.value).then((res) => {
         setSearchResults(res);
       });
     }
   };
 
+  const handleClickOnSearch = (e: React.MouseEvent<HTMLElement>): void => {
+    const clickedInSearchBox = inputRef.current?.contains(e.target as HTMLElement);
+
+    if (clickedInSearchBox && resultsRef.current) {
+      resultsRef.current.classList.remove("hidden");
+    }
+  };
+
+  const handleClickOutside = (e: React.MouseEvent<HTMLElement>): void => {
+    const clickedOutsideSearch = !promptRef.current?.contains(e.target as HTMLElement);
+
+    if (clickedOutsideSearch && resultsRef.current) {
+      resultsRef.current.classList.add("hidden");
+    }
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("mousedown", handleClickOnSearch);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOnSearch);
+    };
+  }, []);
+
   return (
     <>
-      <div id="prompt">
-        <label htmlFor="prompt">Search for a track:</label>
-        <input type="text" id="playlist-box" name="playlistbox" onChange={onSearchChange}></input>
-        <ul id="search-results">{searchResults != null && searchResults.map(mapFnc)}</ul>
+      <label htmlFor="prompt">Search for a track:</label>
+      <div ref={promptRef} id="prompt">
+        <input
+          ref={inputRef}
+          type="text"
+          id="playlist-box"
+          name="playlistbox"
+          onChange={onSearchChange}
+        ></input>
+        <ul ref={resultsRef} id="search-results">
+          {searchResults != null && searchResults.map(mapFnc)}
+        </ul>
       </div>
     </>
     //
